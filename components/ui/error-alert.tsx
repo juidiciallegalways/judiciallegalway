fec1416389ddccada910da48dd972002b21646c3
unknown <khatterharpreet@gmail.com>
2025-12-12 02:37:30 +0530

    fix /profile for mobile view

---
+++ components/ui/error-alert.tsx
@@ -0,0 +1,70 @@
+
+}v>
+  ) </di   lert>
+</A    iption>
+  escrlertD/A  <  >
+    on     </Butt  />
+   " "h-4 w-4 className=    <X          >
+      arent"
+  :bg-transp6 p-0 hoverme="h-6 w- classNa      se)}
+     isible(faltIsVse> ick={() = onCl     "
+      e="sm        siz
+    ost""ghant= vari            <Button
+         >
+      </divge}
+    messa{errorInfo.ng> rostitle}:</orInfo.tstrong>{err        <     <div>
+ 
+        ">neeustify-betwms-center j"flex itelassName= cscription <AlertDe       w-4" />
+"h-4 =ssNamercle claAlertCi      <
+      }`}>
+  ber-200'-amk:textdaramber-900  text-mber-500/100/50 bg-aber-50r-am    : 'borde      00' 
+red-2dark:text-0 text-red-9010 red-500/50 bg-00/der-red-5     ? 'bor' 
+     rror.type === 'eerrorInfo
+        ame={`${Alert classN   <t-4">
+   px-4 px-auto ntainer mcoassName="    <div clrn (
+
+  
+  returMessages] typeof errokeyof as roressages[erfo = errorMt errorInnsull
+
+  coturn nor) reble || !errsiVif (!is
+  i
+chParams]) [sear  }
+  },imer)
+  earTimeout(t() => clrn tu
+      re)
+       10000
+      },(false)setIsVisible{
+        meout(() => Ti timer = set
+      constondsfter 10 sec hide aAuto
+      // 
+      true)ble(sVisi  setIParam)
+    or(errorErr
+      setages]) {of errorMesspe keyof tyaserrorParam ges[rorMessaram && errPaif (erro
+    "error")hParams.get(am = searcrParnst erro
+    co) => {((useEffectnull)
+
+  ng | null>(e<strir] = useStat setErroor,rr
+  const [ete(false)eStaussVisible] = isible, setIisV  const [Params()
+arch = useSerchParams
+  const seaorAlert() {rrn Eport functio  }
+}
+
+exonst
+rror" as c  type: "eess.",
+  dmin for accct aage. Contahis pcess teges to acprivilneed admin e: "You 
+    messag Denied", ccesstle: "A   tiuired: {
+ _access_req
+  adminnst
+  }, coarning" as: "w
+    typeccess.",n a admiadmin to gete. Contact roldent  stuithed wreate has been cur profilge: "Yo
+    messaed",e Creat "Profil  title:dmin: {
+  tact_acreated_conle_rofi pes = {
+ errorMessagconst 
+
+i/button"ponents/urom "@/com } fttonrt { Buact"
+impo "lucide-ree, X } fromertCircl Alimport {i/alert"
+mponents/u"@/cotion } from lertDescripAlert, Aport { "
+im"reactfrom seState } seEffect, u { uon"
+importavigati/n "nextomams } frhParseSearcort { unt"
+
+impclie"use 
